16ebc0866fc4fb477e4e096de04b07fd78324619
Thomas Crowe <wcrowe@g.clemson.edu>
2022-08-20 11:26:12 -0400

    Activity needs to be updated.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import { Bars } from 'react-loader-spinner';
+
 
 function App() {
+  const AboutUs = React.lazy(() => import('./components/AboutUs'));
+  const Packages = React.lazy(() => import('./components/Packages'));
+  
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-          <AboutUs />
-          <Packages />
+          <Suspense fallback={<Bars
+              color="#FF00FF"
+              height={200}
+              width={200}
+              style={{margin: "0  auto"}} />}>
+            <AboutUs />
+            <Packages />
+          </Suspense>
       </div>
     </div>
   );
